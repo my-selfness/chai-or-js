78f854f7bc674b92eec2f59df3ef2924fbf7b787
my-selfness <130391605+my-selfness@users.noreply.github.com>
2023-12-25 08:26:38 +0000

    datatype

---
+++ 01-Basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountID = 144225
+let accountEmail = "hitesh@google.com"
+var accountPassword = "1234"
+let accountState;
+accountCity = "Jodhpur"
+
+
+/*
+prefer not to use var
+*/
+// accountID = 2
+accountEmail = "hell@hdfc.com"
+accountPassword = "23232"
+accountCity="Ranchi"
+
+
+
+console.log(accountID);
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
---
+++ 01-Basic/02_datatype.js
@@ -0,0 +1,16 @@
+
+let name="rahul"
+let age =12
+let isLoggedIn = false
+
+
+//number
+//bigint 
+//string = ""
+//boolean => true / false
+//null
+//undefined
+//symbol => unique
+
+
+//object
