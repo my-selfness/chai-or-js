e18763cb15820c805a5efde5f422253d91d2fb5e
my-selfness <130391605+my-selfness@users.noreply.github.com>
2023-12-25 12:54:01 +0000

    summry

---
+++ data-type_summary.js
@@ -0,0 +1,22 @@
+// premitive > 7 type
+
+// string , number, boolean, null, undefined,symbol,bigint
+
+//reference type (non-primitive)
+//Array,object,function
+
+
+const id= Symbol("123")
+const Aid= Symbol("123")
+// console.log(id===Aid)
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (primitive), Heap(Non-Primitive)
+
+
+let myYoutubeName= "Coding Tantra"
+
+
+
