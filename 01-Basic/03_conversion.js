49f57dea1bfaaf14367872e8657bf62e9ee31060
my-selfness <130391605+my-selfness@users.noreply.github.com>
2023-12-25 12:18:31 +0000

    conversion

--- 01-Basic/03_conversion.js
+++ 01-Basic/03_conversion.js
@@ -1,11 +1,11 @@
 let score =true
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueInScore = Number(score)
 
-console.log(typeof valueInScore)
-console.log(valueInScore)
+// console.log(typeof valueInScore)
+// console.log(valueInScore)
 
 
 //"44" = 44
@@ -16,4 +16,13 @@ console.log(valueInScore)
 let isLoggedIn = 1
 
 let is= Boolean(isLoggedIn)
-console.log(is);
+// console.log(is);
+
+
+// ************************** operation****************
+
+let str1= "hello"
+let str2= "ji"
+let str3= str1 + str2
+
+console.log(str3);
