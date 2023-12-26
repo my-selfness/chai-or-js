11e10adbd297590ac1a8de1a472745f9449992d5
my-selfness <130391605+my-selfness@users.noreply.github.com>
2023-12-26 11:44:39 +0000

    scope done

--- 03-Basic/02_scope.js
+++ 03-Basic/02_scope.js
@@ -1,10 +1,34 @@
 
 
 if (true){
-
     let a = 23
     const b=34
     var c=12
 }
 
-console.log(c);
+// console.log(c);
+
+function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtbe"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+
+addOne(5) //run without any error
+function addOne(num){
+    return num +1
+}
+
+// addTwo(5)  //gives error
+const addTwo= function(num){
+    return num+2
+}
+
+addTwo(5) //right
