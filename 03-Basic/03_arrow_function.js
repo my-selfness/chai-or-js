e1c3add0c6b10164bec83b12b2e33e9b19a9275d
kumar's code <130391605+my-selfness@users.noreply.github.com>
2023-12-26 17:25:20 +0000

    arrow funtion done

---
+++ 03-Basic/03_arrow_function.js
@@ -0,0 +1,23 @@
+
+
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(this);
+// }
+
+// const chai= ()=>{
+//     let username = "hitesh"
+//     console.log(this);
+// }
+
+// chai()
+
+
+// const addTwo=(num1,num2)=>{
+//     return num1+num2
+// }
+// const addTwo=(num1,num2)=>num1+num2
+const addTwo=(num1,num2)=>num1+num2
+
+console.log(addTwo(3,2));
