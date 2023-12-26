c8a252d0a58efd3c81ac18ac8997d348e02fc45c
my-selfness <130391605+my-selfness@users.noreply.github.com>
2023-12-26 08:55:49 +0000

    object some

---
+++ 02-Basic/04_object.js
@@ -0,0 +1,40 @@
+// const tinderUser = new Object() //singletan 
+const tinderUser = {}
+tinderUser.id="12ab"
+tinderUser.name="Samy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser ={
+    email:"user@googele.com",
+    fullname:{
+        userfullName:{
+            firstName:"hitesh",
+            lastName:"chaudhary",
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullName.firstName);
+
+const obj1 = {a:1,b:2}
+const obj2 = {c:1,d:2}
+
+// const obj3=Object.assign({},obj1,obj2)
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+const user = [
+    {
+        id:1,
+        email:"hello@gmail.com"
+    }
+]
+
+// console.log(user[0].email);
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
