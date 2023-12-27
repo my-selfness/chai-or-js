f45ee82ee20e5f235453f6ce86cad6feba9933c5
kumar's code <130391605+my-selfness@users.noreply.github.com>
2023-12-27 14:26:03 +0000

    for each done

--- 05-iteration/05_for_each.js
+++ 05-iteration/05_for_each.js
@@ -1,6 +1,21 @@
 const arr =["py","cpp","js"]
 
 
-arr.forEach(element => {
-   console.log(element); 
-});
+// arr.forEach(element => {
+//    console.log(element); 
+// });
+
+const num =[1,2,3,4,5,6]
+
+const newNum = num.filter( (item) => item > 1)
+
+// console.log(newNum);
+
+const myNum = []
+num.forEach(item=>{
+    if(item>2){
+        myNum.push(item)
+    }
+})
+
+console.log(myNum);
