38c432ecb29198ff961b56b97aab5b95f592346e
kumar's code <130391605+my-selfness@users.noreply.github.com>
2023-12-27 06:59:03 +0000

    iife done

---
+++ 03-Basic/04_iife_function.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai() {
+    console.log("Tayar hai chai!!");
+})();
+
+(()=>{
+    console.log("nasta tayar hai!!");
+})();
+
+
+((name)=>{
+    console.log(`Hello ${name}`);
+})("hitesh");
