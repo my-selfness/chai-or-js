a5b837d910093e59ec4666620ff7de83eb152913
kumar's code <130391605+my-selfness@users.noreply.github.com>
2023-12-27 12:25:15 +0000

    if done

---
+++ 04-controls/01_one.js
@@ -0,0 +1,39 @@
+
+
+const isLoggedIn = true
+const temprature =53
+// if(temprature <50){
+// console.log("less than 50");
+// }
+// else{
+//     console.log("Greater than 50");
+// }
+// <, >, <=, >= , !, ==, ===,  !=,
+
+
+const score = 300
+
+// if (score >100) {
+//     const power = "Fly"
+//     console.log(`user power : ${power}`);
+// }
+
+
+const balance = 1000
+
+// if (balance>5000) console.log("test");
+
+// if (balance<500) {
+//     console.log("less than 500");
+// }
+// else if (balance<100) {
+//     console.log("less than 1000");
+// } else if (balance<2000) {
+//     console.log("less than 2000")
+// }
+
+
+
+// &&  -- and
+// || -- or
+// !  --not
