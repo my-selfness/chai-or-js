97d402a0f12d184fa414907a64dac2e3911cfdc9
kumar's code <130391605+my-selfness@users.noreply.github.com>
2023-12-27 13:18:34 +0000

    for loop done

---
+++ 05-iteration/01_loops.js
@@ -0,0 +1,22 @@
+// for 
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);    
+// }
+
+
+let array = ["flash", "batman", "superman"]
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+    
+// }
+
+for (let i = 1; i < 20; i++) {
+
+    if(i==10){
+        console.log(i);
+        continue
+    }
+    console.log(i);
+}
