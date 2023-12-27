d0ee0958750080c3f39ef36a19b92bd3ca70d312
kumar's code <130391605+my-selfness@users.noreply.github.com>
2023-12-27 12:54:35 +0000

    controls done

---
+++ 04-controls/02_switch.js
@@ -0,0 +1,15 @@
+
+
+const month = 2
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+
+    default:
+        console.log("invalid");
+        break;
+}
---
+++ 04-controls/03_truthy.js
@@ -0,0 +1,16 @@
+
+
+// nullish coalescing operator (??) : null undefined
+
+let val1;
+val1= 20 ?? 43
+
+console.log(val1);
+
+// Ternary Operator 
+
+// condition ? true : false
+
+const iceTeaPrice = 233
+
+iceTeaPrice <=500 ? console.log("less") : console.log("more");;
